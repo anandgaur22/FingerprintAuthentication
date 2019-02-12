2afcf9d6b61858d774465f350e5ba7bfbbc0c4f0
gyanendra <gyanendra.jha@in.accemy.com>
2019-02-12 12:31:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fingerprintauthentication/FingerprintHandler.java
@@ -0,0 +1,71 @@
+package com.example.fingerprintauthentication;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.CancellationSignal;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * Created by anand
+ */
+public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
+
+
+    private Context context;
+
+
+    // Constructor
+    public FingerprintHandler(Context mContext) {
+        context = mContext;
+    }
+
+
+    public void startAuth(FingerprintManager manager, FingerprintManager.CryptoObject cryptoObject) {
+        CancellationSignal cancellationSignal = new CancellationSignal();
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
+    }
+
+
+    @Override
+    public void onAuthenticationError(int errMsgId, CharSequence errString) {
+        this.update("Fingerprint Authentication error\n" + errString, false);
+    }
+
+
+    @Override
+    public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+        this.update("Fingerprint Authentication help\n" + helpString, false);
+    }
+
+
+    @Override
+    public void onAuthenticationFailed() {
+        this.update("Fingerprint Authentication failed.", false);
+    }
+
+
+    @Override
+    public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+        this.update("Fingerprint Authentication succeeded.", true);
+        Toast.makeText(context, "Finger print success", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void update(String e, Boolean success){
+        TextView textView = (TextView) ((Activity)context).findViewById(R.id.errorText);
+        textView.setText(e);
+        if(success){
+            textView.setTextColor(ContextCompat.getColor(context,R.color.colorPrimaryDark));
+        }
+    }
+}
